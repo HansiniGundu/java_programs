a598c88cc795ddbff63860a8d79d0e41dc814f07
Hansinij1 <154776498+Hansinij1@users.noreply.github.com>
2024-08-04 15:01:24 +0530

    Add files via upload

---
+++ id_operations.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class id_operations{
+   
+    public static void main(String args[]){
+      
+       Scanner sc = new Scanner(System.in);
+       int x = sc.nextInt();
+       
+
+       System.out.println(++x);
+       System.out.println(x++);
+       System.out.println(--x);
+       System.out.println(x--);
+    }
+}
+
