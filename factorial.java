799f7c61cd1c2267a201e432a8c775e9c6c4dcd1
Hansinij1 <154776498+Hansinij1@users.noreply.github.com>
2024-08-04 15:00:19 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class factorial{
+    
+    public static void main(String args[]){
+        
+        Scanner sc = new Scanner(System.in);
+        int i,fact = 1;
+        int n = sc.nextInt();
+        for(i=1;i<=n;i++){
+           
+           fact = fact*i;
+        }
+        System.out.println(fact);
+   }
+}
+
+        
