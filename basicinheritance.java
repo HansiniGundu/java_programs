1377478846231fcb72d7062fd1e3d463ec24195a
Hansini Gundu <154776498+HansiniGundu@users.noreply.github.com>
2024-08-22 12:33:39 +0530

    Add files via upload

---
+++ basicinheritance.java
@@ -0,0 +1,21 @@
+package javapractice;
+
+class Animal {
+ void eat() {
+     System.out.println("This animal eats food.");
+ }
+}
+
+class Dog extends Animal {
+ void bark() {
+     System.out.println("The dog barks.");
+ }
+}
+
+public class basicinheritance {
+ public static void main(String[] args) {
+     Dog myDog = new Dog();
+     myDog.eat();  
+     myDog.bark(); 
+ }
+}
